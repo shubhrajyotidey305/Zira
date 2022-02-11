af23b640ee7182a745ca2864a4a6cc972549c9b3
shubhrajyotidey305 <shubhrajyotidey305@gmail.com>
2022-02-11 21:45:46 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zira
+A virtual assistant
---
+++ zira.py
@@ -0,0 +1,122 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import cv2
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am Zira. Please tell me how may I help you")
+
+def takeCommand():
+    #takes microphone input and returns string input
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.login('email@gmail.com', 'password')
+    server.sendmail('email@gmail.com', to, content)
+    server.close()
+
+if __name__ == '__main__':
+    wishMe()
+
+    while True:
+        query = takeCommand().lower()
+
+        #logic for executing task
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'play music' in query:
+            music_dir = 'D:\\songs'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+        
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codepath = "D:\\Microsoft VS Code\\Code.exe"
+            os.startfile(codepath)
+
+        elif 'open camera' in query:
+            cap = cv2.VideoCapture(0)
+            while True:
+                ret, img = cap.read()
+                cv2.imshow('webcam', img)
+                k = cv2.waitKey(50)
+                if k == 27:
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
+
+        elif 'email to me' in query:
+            try:
+                speak("what should I say?")
+                content = takeCommand()
+                to = "shubhrajyotidey305@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend Shubhra, I am not able to send this email")
+        
+        elif 'bye' in query:
+            exit()
